a233d6d641a46015eee204b68467861195b19a9e
Dharma Thadi <dreams.dharam@gmail.com>
2018-11-12 22:52:47 -0600

    adding cubemaster code to project

---
+++ CubeGenerator/src/main/java/com/javatpoint/App.java
@@ -0,0 +1,18 @@
+package com.javatpoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	sayHelloWorld();
+    }
+    public static void sayHelloWorld() {
+    	System.out.println("Hello World!" );
+    	return ;
+    }
+}
